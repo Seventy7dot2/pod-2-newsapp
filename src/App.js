131c450f47d77754783a003142c4cbfe1ffcdf5e
Debajyoti <chatterjeedebajyoti254@gmail.com>
2024-03-23 16:47:32 +0530

    changing the app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import NavInshort from "./components/NavInshort";
-import NewsContent from "./components/NewsContent/NewsContent";
+import Footerz from "./components/Footer/footer";
+import nav from "./components/navbar";
+import head from "./components/header2/header2";
+
 
 
 function App() {
-  const [newsArray, setNewsArray] = useState([]);
-  const [newsResults, setNewsResults] = useState();
-  const [loadMore, setLoadMore] = useState(20);
-  const [category, setCategory] = useState("general");
-
-  console.log(process.env);
-
-  const newsApi = async () => {
-    try {
-      
-    
-        const news = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&category=${category}&pageSize=${loadMore}`
-      );
-      // console.log(news);
-      setNewsArray(news.data.articles);
-      setNewsResults(news.data.totalResults);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    newsApi();
-    // eslint-disable-next-line
-  }, [newsResults, loadMore, category]);
 
   return (
     <div className="App" id="#home">
-      <NavInshort setCategory={setCategory} />
-      {newsResults && (
-        <NewsContent
-          newsArray={newsArray}
-          newsResults={newsResults}
-          loadMore={loadMore}
-          setLoadMore={setLoadMore}
-        />
-      )}
-      <Footer />
+      <nav setCategory={setCategory} />
+        
+      <Footerz />
     </div>
   );
 }
